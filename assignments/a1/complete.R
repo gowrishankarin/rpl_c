a0caaa7e15b34fc4067e3a6ab1a40c35abcc6cb2
Gowri Shankar <gowrishankar.m@prodapt.com>
2015-04-10 15:37:48 +0530

    Complete cases completed

---
+++ assignments/a1/complete.R
@@ -0,0 +1,45 @@
+complete <- function(directory, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return a data frame of the form:
+    ## id nobs
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+    
+    # Get file names
+    files <- list.files(path=directory)
+    data <- list()
+    
+    sum <- 0
+    count <- 0
+    
+    complete_cases <- data.frame()
+    
+    # Read required files
+    for(i in id) {
+        aFile <- paste(directory, "/", files[[i]], sep="")
+        data <- read.csv(aFile)
+        
+        good <- complete.cases(data)
+        valids <- data[good,]
+        
+        size <- dim(valids)[[1]]
+        
+        complete_cases[i,1] = i
+        complete_cases[i,2] = size
+        
+    }
+    good <- complete.cases(complete_cases)
+    valids <- complete_cases[good,]
+    colnames(valids) <- c("id", "nobs")
+    rownames(valids)<- c(1:dim(valids)[[1]])
+    
+    valids
+}
