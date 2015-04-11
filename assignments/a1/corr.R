cf1a4e18dca2f788a6c3f63809f44e31a48d73dd
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-04-11 21:29:20 +0530

    correlation problems implemented

--- assignments/a1/corr.R
+++ assignments/a1/corr.R
@@ -10,13 +10,30 @@ corr <- function(directory, threshold = 0) {
     ## Return a numeric vector of correlations
     
     ## Get complete cases
+  
+    files <- list.files(path=directory)
+    data <- data.frame()
+ 
     complete_cases = complete(directory)
     size = dim(complete_cases)[[1]]
     
-    for(i in size) {
-        if(complete_cases["nobs"] > threshold) {
-            ## Do correlation
+    corr_matrix <- vector()
+    count <- 0
+    
+    for(i in 1:size) {
+        if(complete_cases[i,"nobs"] > threshold) {
             
+            # Get file identifier
+            file_name <- paste(directory, "/", files[[i]], sep="")
+            data <- read.csv(file_name)[2:3]
+            good <- complete.cases(data)
+            good_data <- data[good,]
+            cor_val <- cor(good_data["sulfate"], good_data["nitrate"])
+            count <- count + 1
+            corr_matrix[count] <- cor_val
         }
     }
+
+    corr_matrix
+    
 }
