4ef6b720186fcb18517a9c2a7c77574446504737
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-04-23 22:29:31 +0530

    Hospital data analysis

---
+++ assignments/a4/best.R
@@ -0,0 +1,10 @@
+best <- function(state, outcome) {
+    ## Read outcome data
+    outcome <- read.csv("./data/outcome-of-care-measures.csv")
+    
+    ## Check that sate and outcome are valid
+    
+    ## Return hospital name in that state with lowest 30-day death
+    ## rate
+    
+}
